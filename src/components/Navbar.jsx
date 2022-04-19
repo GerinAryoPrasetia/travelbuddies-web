5df44ef304331f47147508798e11bc340895ae8f
GerinAryoPrasetia <gerinaryo14@gmail.com>
2022-04-19 10:49:09 +0700

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isAuth, setIsAuth] = useState(false);
@@ -18,7 +19,7 @@ const Navbar = () => {
                 type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Login
+                <Link to={"/login"}>Login</Link>
               </button>
             ) : (
               <img
